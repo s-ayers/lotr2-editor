234aa3db478666dd12bcf7baf8c515580b1b6e73
Steve Ayers <steveneayers@gmail.com>
2019-12-08 08:20:39 -0600

    Update diff tool

--- bin/diff.js
+++ bin/diff.js
@@ -119,11 +119,43 @@ headerLocation = (offset) => {
 }
 
 inventoryLocation = (offset) => {
-  return 'Inventory';
+  
+  var location = 'Inventory:';
+  if (offset > 84863 && offset < 84864) {
+    location += 'Crowns';
+  } else if (offset > 84871 && offset < 84876) {
+    location += 'Iron';
+  } else if (offset > 84879 && offset < 84884) {
+    location += 'Stone';
+  } else if (offset > 84888 && offset < 84892) {
+    location += 'Wood';
+  } else {
+    location += 'Unknown';
+  }
+
+  return location;
 }
 
 armoryLocation = (offset) => {
-  return 'Armory';
+  var location = 'Armory:';
+
+  if (offset > 84903 && offset < 84908) {
+    location += 'Crossbows';
+  } else if (offset > 84907 && offset < 84912) {
+    location += 'Maces';
+  } else if (offset > 84911 && offset < 84916) {
+    location += 'Swords';
+  } else if (offset > 84915 && offset < 84920) {
+    location += 'Pikes';
+  } else if (offset > 84919 && offset < 84924) {
+    location += 'Bows';
+  } else if (offset > 84923 && offset < 84928) {
+    location += 'Knights';
+  } else {
+    location += 'Unknown';
+  }
+
+  return location;
 }
 
 countyLocation = (offset) => {
@@ -136,7 +168,10 @@ countyLocation = (offset) => {
 armyLocation = (offset) => {
   var army = 115 - Math.floor((147658 - offset) / 420);
 
-  return printf('Army:%03d', army);
+  var location = printf('Army:%03d:', army);
+
+
+  return location;
 }
 
 footLocation = (offset) => {
